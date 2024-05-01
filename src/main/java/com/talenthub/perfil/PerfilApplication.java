89451ed695c6df51d3bd7acf1cdb8eccf49b6986
ticaso <ticaso999@gmail.com>
2024-05-01 18:02:27 -0500

    INITIAL

---
+++ src/main/java/com/talenthub/perfil/PerfilApplication.java
@@ -0,0 +1,13 @@
+package com.talenthub.perfil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/talenthub/perfil/PerfilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talenthub.perfil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
